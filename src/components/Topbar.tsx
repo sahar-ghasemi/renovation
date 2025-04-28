b313105f4b551c24d70a9bda132f0464e8827370
sahar <saharghasemi7711@gmail.com>
2025-04-28 23:03:35 +1000

    chore: add some motion and change color for text

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -14,8 +14,10 @@ const Topbar = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  const iconStyle = "w-5 h-5 text-gray";
-
+  const iconStyle = "w-5 h-5 text-ivory";
+  const letters = Array.from(
+    " You can easily cover your stone costs from our stone menu!"
+  );
   return (
     <div
       className={`transition-all duration-500 text-sm text-black
@@ -24,9 +26,34 @@ const Topbar = () => {
     >
       {/* »ثبف side: Promotional text */}
       <div className="text-gray flex gap-2 items-center font-semibold text-sm sm:text-base text-right">
-        <Gem className="w-5 h-5 text-sand" />
-        <span className="mr-2">
-          You can easily cover your stone costs from our stone menu!
+        <motion.div
+          animate={{
+            y: [0, -4, 0],
+          }}
+          transition={{
+            duration: 1, // مثلا هر ۴ ثانیه یه دور
+            repeat: Infinity, // همیشه بچرخه
+            repeatType: "loop", // لوپ شه
+          }}
+        >
+          <Gem className="w-5 h-5 text-ivory" />
+        </motion.div>
+        <span className="mr-2 text-ivory flex ">
+          {letters.map((letter, index) => (
+            <motion.span
+              key={index}
+              initial={{ y: 0 }}
+              animate={{ y: [0, -6, 0] }}
+              transition={{
+                duration: 1,
+                ease: "easeInOut",
+                delay: index * 0.05, // هر حرف کمی دیرتر شروع کنه
+              }}
+              className="text-ivory font-semibold whitespace-pre"
+            >
+              {letter}
+            </motion.span>
+          ))}{" "}
         </span>
       </div>
       {/* Right side: Social icons */}
@@ -40,7 +67,7 @@ const Topbar = () => {
           className="flex items-center gap-1 cursor-pointer"
         >
           <Instagram className={iconStyle} />
-          <span className="text-gray hidden sm:inline">Instagram</span>
+          <span className="text-ivory hidden sm:inline">Instagram</span>
         </motion.a>
 
         <motion.a
@@ -52,7 +79,7 @@ const Topbar = () => {
           className="flex items-center gap-1 cursor-pointer"
         >
           <Phone className={iconStyle} />
-          <span className="text-gray hidden sm:inline">WhatsApp</span>
+          <span className="text-ivory hidden sm:inline">WhatsApp</span>
         </motion.a>
       </div>
     </div>
